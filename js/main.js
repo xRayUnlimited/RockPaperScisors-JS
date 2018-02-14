ff684ba1e30d87e4b0e527be9b61596ac8b24b8e
Ray Suyoto <ray.suy@icloud.com>
2018-02-13 17:14:57 -0700

    small js changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+var user;
+var computer;
+var winner;
+
 function startGame(e) {
-  var choice = e.target.id
-  console.log(choice)
+  user = e.target.id
+  computerChoice();
+  checkWin();
+  displayResults();
+}
+
+function computerChoice() {
+  var choices = ['rock', 'paper', 'scissors']
+  var index = Math.floor(Math.random() * 3)
+  console.log(choices[index])
+}
+
+function checkWin() {
+  console.log('checking for win')
+}
+
+function displayResults() {
+  console.log('displaying results')
 }
 
 var choices = document.getElementsByClassName('choice')
+
 for (var i in choices) {
   try {
     choices[i].addEventListener('click', startGame)
@@ -11,3 +32,10 @@ for (var i in choices) {
     //First load
   }
 }
+
+var start = document.getElementById('start')
+start.addEventListener('click', function() {
+  start.className = "hide"
+  var game = document.getElementById('game')
+  game.className = 'container'
+});
